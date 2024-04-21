a0a93c877fd9dbf2c90580e1abf0da558fca1838
indextree <brilliantepoch22@gmail.com>
2024-04-21 18:55:42 +0900

    Implement special fft

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -59,7 +59,128 @@ fn fft(coeff: &mut [Complex], invert: bool) {
     }
 }
 
-fn poly_mul(n: usize, x: &[f64], m: usize, y: &[f64]) -> Vec<f64> {
+fn is_power_of_two(n: usize) -> bool {
+    n != 0 && (n & (n - 1)) == 0
+}
+
+fn bit_reverse_value(mut x: usize, mut num_bits: usize) -> usize {
+    let mut rev_x = 0;
+    while num_bits > 0 {
+        rev_x <<= 1;
+        rev_x |= x & 1;
+        x >>= 1;
+        num_bits -= 1;
+    }
+    rev_x
+}
+
+fn get_bit_reverse_array(a: &[Complex], n: usize) -> Vec<Complex> {
+    assert!(is_power_of_two(n));
+
+    let mut r = vec![Complex(0.0, 0.0); n];
+    for i in 0..n {
+        let bit_reverse_i = bit_reverse_value(i, (n as f64).log2() as usize);
+        r[bit_reverse_i] = a[i];
+    }
+
+    r
+}
+
+fn find_mth_root_of_unity(m: usize) -> Complex {
+    let tmp = 2.0 * std::f64::consts::PI / m as f64;
+    let zeta = Complex(tmp.cos(), tmp.sin());
+    zeta
+}
+
+fn get_psi_powers(m: usize) -> Vec<Complex> {
+    let psi = find_mth_root_of_unity(m);
+    let mut psi_powers = Vec::with_capacity(m);
+    let mut tmp = Complex(0.0, 0.0);
+    for i in 0..m {
+        psi_powers.push(tmp);
+        tmp = tmp * psi;
+    }
+    psi_powers.push(psi_powers[0]);
+
+    psi_powers
+}
+
+fn get_rot_group(n_half: usize, m: usize) -> Vec<usize> {
+    let mut p = 1;
+    let mut rot_group = Vec::with_capacity(n_half);
+    for _ in 0..n_half {
+        rot_group.push(p);
+        p *= 5;
+        p %= m;
+    }
+
+    rot_group
+}
+
+fn special_fft(a: &Vec<Complex>, n: usize, m: usize) -> Vec<Complex> {
+    assert_eq!(a.len(), n);
+    assert!(is_power_of_two(n));
+
+    let mut a = get_bit_reverse_array(a, n);
+    let psi_powers = get_psi_powers(m);
+    let rot_group = get_rot_group(m >> 2, m);
+
+    let mut length_n = 2;
+    while length_n <= n {
+        for i in (0..n).step_by(length_n) {
+            let lenh = length_n >> 1;
+            let lenq = length_n << 2;
+            let gap = m / lenq;
+            for j in (0..lenh).step_by(1) {
+                let idx = (rot_group[j] % lenq) * gap;
+                let u = a[i + j];
+                let mut v = a[i + j + lenh];
+                v = v * psi_powers[idx];
+                a[i + j] = u + v;
+                a[i + j + lenh] = u - v;
+            }
+        }
+        length_n *= 2;
+    }
+
+    a
+}
+
+fn special_ifft(a: &Vec<Complex>, n: usize, m: usize) -> Vec<Complex> {
+    assert_eq!(a.len(), n);
+    assert!(is_power_of_two(n));
+
+    let mut a = a.to_vec();
+
+    let mut length_n = n;
+    let psi_powers = get_psi_powers(m);
+    let rot_group = get_rot_group(m >> 2, m);
+
+    while length_n >= 1 {
+        for i in (0..n).step_by(length_n) {
+            let lenh = length_n >> 1;
+            let lenq = length_n << 2;
+            let gap = m / lenq;
+            for j in (0..lenh) {
+                let idx = (lenq - (rot_group[j] % lenq)) * gap;
+                let u = a[i + j] + a[i + j + lenh];
+                let mut v = a[i + j] - a[i + j + lenh];
+                v = v * psi_powers[idx];
+                a[i + j] = u;
+                a[i + j + lenh] = v;
+            }
+        }
+        length_n >>= 1;
+    }
+
+    a = get_bit_reverse_array(&a, n);
+
+    // Multiply by 1/n and return
+    a.iter().map(|&x| Complex(x.0 / (n as f64), x.1 / (n as f64))).collect()
+}
+
+
+fn poly_mul(n: usize, x: &Vec<f64>, m: usize, y: &Vec<f64>) -> Vec<f64> {
     assert_eq!(n, x.len());
     assert_eq!(m, y.len());
 
@@ -70,11 +191,20 @@ fn poly_mul(n: usize, x: &[f64], m: usize, y: &[f64]) -> Vec<f64> {
     x.resize(len, Complex(0., 0.));
     y.resize(len, Complex(0., 0.));
 
+    let mut xx = special_fft(&mut x, len, len * 2);
     fft(&mut x, false);
+    let mut yy = special_fft(&mut y, len, len * 2);
     fft(&mut y, false);
 
     x.iter_mut().zip(&y).for_each(|(xi, &yi)| *xi = *xi * yi);
+    xx.iter_mut().zip(&yy).for_each(|(xxi, &yyi)| *xxi = *xxi * yyi);
+
     fft(&mut x, true);
+    let mut z = special_ifft(&mut xx, len, len * 2);
+
+    for zi in &z {
+        println!("{} {}", zi.0, zi.1);
+    }
 
     if cfg!(debug_assertions) {
         println!("Results of FFT & iFFT:");
